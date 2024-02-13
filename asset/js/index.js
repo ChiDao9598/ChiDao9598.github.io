383afb9fbc449a27b72038b8b58bc0a8fdea1f17
Chi <Chi.DaoXuan@vn.bosch.com>
2024-02-14 01:32:23 +0700

    changes

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,4 +1,11 @@
-$(document).ready(function() {})
+$(document).ready(function() {
+    let startDay = new Date("02/05/2024");
+    let today = new Date();
+    let Difference_In_Time = today.getTime() - startDay.getTime();
+    let Difference_In_Days = Math.round (Difference_In_Time / (1000 * 3600 * 24));
+    console.log(Difference_In_Days)
+    $(".dayCount").append(Difference_In_Days + " days been together");
+})
 
 function showContentEvent() {
     $(".showContent").toggleClass("hide")
